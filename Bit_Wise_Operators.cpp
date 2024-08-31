9a988fb1e2113752f7457f456a8e94ac6e5b22c6
Anand8726 <anandnair2218@gmail.com>
2024-08-31 23:37:47 +0530

    intial commit

---
+++ Bit_Wise_Operators.cpp
@@ -0,0 +1,28 @@
+//BITWISE OPERATORS
+//Considering values of X and Y as 11 and 7 respectively.
+#include<iostream>
+using namespace std;
+int main(){
+
+    int x = 11 , y = 7 , z;
+
+    z = x & y ;
+    cout<<" AND OPERATION = "<<z<<endl;
+    
+    z = x | y ;
+    cout<<" OR OPERATION = "<<z<<endl;
+    
+    z = x ^ y ;
+    cout<<" X-OR OPERATION = "<<z<<endl;
+    
+    z = ~ x ;
+    cout<<" NOT OPERATION = "<<z<<endl;
+
+    char a = 20 , b ;
+    
+    b = a << 1;
+    cout << " Left-Shift by 1 = "<<(int)b<<endl;
+    
+    b = a >> 1 ;
+    cout<<" Right-Shift by 1 = "<<(int)b;
+}
